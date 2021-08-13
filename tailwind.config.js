3b2b509a11203f6221292ab79d3136e49147fd3a
Ognjen <opejovic@gmail.com>
2021-08-13 16:28:33 +0200

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        pgtGreen: '#254C5A',
+        pgtGray: '#343A40'
+      },
+      height: {
+        pgtSection: '563px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
